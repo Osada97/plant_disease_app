c0144ca8622e99102c830ccfff7abd1b8c3f99d3
Osada97 <osadamanohara55@gmail.com>
2022-03-25 17:30:35 +0530

    added svgs to welcome page

--- components/WelcomeComponents.js
+++ components/WelcomeComponents.js
@@ -1,24 +1,25 @@
+import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import GlobalStyles from "../utils/GlobalStyles";
-import React from "react";
 import useWindowDimensions from "react-native/Libraries/Utilities/useWindowDimensions";
+import { WelcomeOne, WelcomeTwo, WelcomeThree } from "../assets/svgs/Welcome";
 
 const WelcomeComponents = ({ item }) => {
   const { width } = useWindowDimensions();
 
   return (
     <View style={[styles.container, { width }]}>
-      <View style={styles.imageRow}>
+      <View style={styles.imRowContainer}>
         {item.id === 1 ? (
-          <ImageSectionOne />
+          <ImageSectionOne width={width} />
         ) : item.id === 2 ? (
-          <ImageSectionTwo />
+          <ImageSectionTwo width={width} />
         ) : (
-          <ImageSectionThree />
+          <ImageSectionThree width={width} />
         )}
       </View>
       <View style={styles.TextRow}>
-        <View style={styles.mainTextRow}>
+        <View>
           <Text style={styles.mainText}>{item.title}</Text>
         </View>
         <View>
@@ -29,62 +30,77 @@ const WelcomeComponents = ({ item }) => {
   );
 };
 
-const ImageSectionOne = () => {
+const ImageSectionOne = ({ width }) => {
   return (
     <>
-      <View>
-        <Image
-          style={styles.image}
-          source={require("../assets/jpgs/welcome1.jpg")}
-          resizeMode="contain"
-        />
+      <View style={styles.imageRow}>
+        <View>
+          <Image
+            style={styles.image}
+            source={require("../assets/jpgs/welcome1.jpg")}
+            resizeMode="contain"
+          />
+        </View>
+        <View>
+          <Image
+            style={[styles.image, styles.image2]}
+            source={require("../assets/jpgs/welcome2.jpg")}
+            resizeMode="contain"
+          />
+        </View>
+        <View>
+          <Image
+            style={styles.image}
+            source={require("../assets/jpgs/welcome3.jpg")}
+            resizeMode="contain"
+          />
+        </View>
       </View>
-      <View>
-        <Image
-          style={[styles.image, styles.image2]}
-          source={require("../assets/jpgs/welcome2.jpg")}
-          resizeMode="contain"
-        />
-      </View>
-      <View>
-        <Image
-          style={styles.image}
-          source={require("../assets/jpgs/welcome3.jpg")}
-          resizeMode="contain"
-        />
+      <View style={[styles.svgSection, { width, aspectRatio: 1 }]}>
+        <WelcomeOne />
       </View>
     </>
   );
 };
-const ImageSectionTwo = () => {
+const ImageSectionTwo = ({ width }) => {
   return (
     <>
-      <View>
-        <Image
-          style={styles.image}
-          source={require("../assets/jpgs/welcome1.jpg")}
-          resizeMode="contain"
-        />
+      <View style={styles.imageRow}>
+        <View>
+          <Image
+            style={styles.image}
+            source={require("../assets/jpgs/welcome1.jpg")}
+            resizeMode="contain"
+          />
+        </View>
+        <View>
+          <Image
+            style={[styles.image, styles.image2]}
+            source={require("../assets/jpgs/welcome2.jpg")}
+            resizeMode="contain"
+          />
+        </View>
       </View>
-      <View>
-        <Image
-          style={[styles.image, styles.image2]}
-          source={require("../assets/jpgs/welcome2.jpg")}
-          resizeMode="contain"
-        />
+      <View style={[styles.svgSection, { width, aspectRatio: 1 }]}>
+        <WelcomeTwo />
       </View>
     </>
   );
 };
-const ImageSectionThree = () => {
+const ImageSectionThree = ({ width }) => {
   return (
     <>
-      <View>
-        <Image
-          style={styles.image}
-          source={require("../assets/jpgs/welcome1.jpg")}
-          resizeMode="contain"
-        />
+      <View style={styles.imageRow}>
+        <View>
+          <Image
+            style={styles.image}
+            source={require("../assets/jpgs/welcome1.jpg")}
+            resizeMode="contain"
+          />
+        </View>
+      </View>
+      <View style={[styles.svgSection, { width, aspectRatio: 1 }]}>
+        <WelcomeThree />
       </View>
     </>
   );
@@ -96,19 +112,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  imRowContainer: {
+    flex: 0.5,
+    position: "relative",
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 25,
+  },
   imageRow: {
+    position: "relative",
+    zIndex: 3,
     flexDirection: "row",
     justifyContent: "space-between",
     paddingTop: 60,
     paddingBottom: 40,
   },
+  svgSection: {
+    position: "absolute",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    top: -25,
+    left: 0,
+  },
   image: {
     width: 130,
-    height: 180,
+    height: 200,
     borderRadius: 25,
   },
   image2: {
     marginTop: 40,
+  },
+  TextRow: {
+    flex: 0.5,
   },
   mainText: {
     textAlign: "center",
@@ -121,19 +156,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: "#777",
     lineHeight: 25,
-  },
-  ButtonRow: {
-    marginTop: 40,
-  },
-  Button: {
-    width: "100%",
-    backgroundColor: GlobalStyles.buttonColor,
-    padding: 15,
-    borderRadius: 10,
-  },
-  ButtonText: {
-    textAlign: "center",
-    color: "#fff",
-    fontSize: 16,
   },
 });
