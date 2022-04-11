6c1a2811091d1624369b8c8ab5cc7d260286245d
Osada97 <osadamanohara55@gmail.com>
2022-04-12 02:34:08 +0530

    added login page and error handelling

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -6,20 +6,89 @@ import {
   TouchableWithoutFeedback,
   TextInput,
 } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GlobalStyles from "../utils/GlobalStyles";
+import ErrorModel from "../utils/ErrorModel";
 
 const LoginForm = ({ navigation }) => {
+  const [loginForm, setLoginForm] = useState({ userName: "", password: "" });
+  const [errors, setErrors] = useState({ userName: "", password: "" });
+  const [isModel, setIsModel] = useState(false);
+  const [checkError, setCheckError] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  useEffect(() => {
+    //check if there is an any errors and show the model
+    if (Object.keys(errors).length > 0) {
+      if (errors[Object.keys(errors)[0]] !== "") {
+        setIsModel(true);
+      }
+    }
+  }, [checkError]);
+
+  useEffect(() => {
+    //check if there is an any errors and show the model
+    if (Object.keys(errors).length === 0 && isSubmit) {
+      handelSubmit();
+    }
+  }, [errors]);
+
+  const setValues = (text, name) => {
+    setLoginForm({ ...loginForm, [name]: text });
+  };
+
+  const onSubmit = () => {
+    let formErrors = {};
+
+    if (loginForm.userName.trim().length === 0) {
+      formErrors.userName = "Please Enter UserName";
+    }
+    if (loginForm.password.trim().length === 0) {
+      formErrors.password = "Please Enter Password";
+    }
+    setErrors({ ...formErrors });
+    setCheckError(!checkError);
+    setIsSubmit(true);
+  };
+
+  const onFocus = (type) => {
+    if (errors[type] && errors[type] !== "") {
+      setErrors({ ...errors, [type]: "" });
+    }
+  };
+
+  const handelSubmit = () => {
+    console.log("handel submit");
+  };
+
   return (
     <View style={styles.form}>
+      {isModel && (
+        <ErrorModel
+          title="Login Failed"
+          msg={errors}
+          type="Error"
+          setIsModel={setIsModel}
+        />
+      )}
       <View style={styles.inputField}>
-        <TextInput style={styles.inputStyle} placeholder="Username" />
+        <TextInput
+          icon="mail"
+          style={[styles.inputStyle, errors.userName && styles.inputStyleError]}
+          placeholder="Username"
+          onChangeText={(text) => setValues(text, "userName")}
+          onFocus={() => onFocus("userName")}
+          value={loginForm.userName}
+        />
       </View>
       <View style={styles.inputField}>
         <TextInput
           secureTextEntry={true}
-          style={styles.inputStyle}
+          style={[styles.inputStyle, errors.password && styles.inputStyleError]}
           placeholder="Password"
+          onChangeText={(text) => setValues(text, "password")}
+          onFocus={() => onFocus("password")}
+          value={loginForm.password}
         />
       </View>
       <View style={styles.endLine}>
@@ -28,7 +97,7 @@ const LoginForm = ({ navigation }) => {
         </TouchableWithoutFeedback>
       </View>
       <View style={styles.formButton}>
-        <TouchableHighlight style={styles.button}>
+        <TouchableHighlight style={styles.button} onPress={onSubmit}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableHighlight>
         <View style={styles.bottomTextSec}>
@@ -68,6 +137,9 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     width: "100%",
     borderRadius: 8,
+  },
+  inputStyleError: {
+    backgroundColor: "#fff5f5",
   },
   endLine: {
     alignItems: "flex-end",
---
+++ utils/ErrorModel.js
@@ -0,0 +1,120 @@
+import {
+  View,
+  Text,
+  Modal,
+  Dimensions,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faCircleExclamation,
+  faCheckCircle,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import GlobalStyles from "./GlobalStyles";
+
+const deviceWidth = Dimensions.get("window").width;
+
+const ErrorModel = ({ title = "", msg, type = "Error", setIsModel }) => {
+  if (typeof msg !== "object") {
+    console.error("Message parameter is not equal to Object");
+  }
+
+  //get one key value pair of object
+  if (msg[Object.keys(msg)[0]] !== "") {
+    msg = msg[Object.keys(msg)[0]];
+  }
+
+  return (
+    <Modal
+      animationType="fade"
+      visible={true}
+      transparent
+      hardwareAccelerated={true}
+      onRequestClose={() => setIsModel(false)}
+    >
+      <TouchableOpacity
+        style={styles.container}
+        onPress={() => setIsModel(false)}
+      >
+        <View style={styles.alertContainer}>
+          <View style={styles.icon}>
+            <FontAwesomeIcon
+              icon={type === "Error" ? faCircleExclamation : faCheckCircle}
+              size={23}
+              color={type === "Error" ? "#ff1f57" : "#2dc254"}
+            />
+          </View>
+          <View style={styles.textContainer}>
+            {title !== "" ? (
+              <Text style={styles.errorTitle}>{title || ""}</Text>
+            ) : (
+              ""
+            )}
+            <Text
+              style={[
+                styles.errorSubTitle,
+                { color: type === "Error" ? "#ff1f57" : "#2dc254" },
+              ]}
+            >
+              {msg}
+            </Text>
+          </View>
+          <TouchableOpacity
+            style={styles.iconClose}
+            onPress={() => setIsModel(false)}
+          >
+            <FontAwesomeIcon
+              icon={faXmark}
+              size={23}
+              color={type === "Error" ? "#ff1f57" : "#2dc254"}
+            />
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    </Modal>
+  );
+};
+
+export default ErrorModel;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: deviceWidth,
+    alignItems: "center",
+    paddingVertical: 15,
+  },
+  alertContainer: {
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 5,
+    paddingVertical: 10,
+    borderRadius: 12,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  icon: {
+    flex: 0.1,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  textContainer: {
+    flex: 0.8,
+  },
+  iconClose: {
+    flex: 0.1,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  errorTitle: {
+    fontSize: 16,
+    fontFamily: GlobalStyles.mediumFonts,
+  },
+  errorSubTitle: {
+    fontSize: 15,
+    fontFamily: GlobalStyles.customFonts,
+    marginTop: 3,
+  },
+});
