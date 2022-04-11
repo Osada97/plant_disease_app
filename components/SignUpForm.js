d0fdc0e15a9898d5e0de6f54c317c5b85d6da667
Osada97 <osadamanohara55@gmail.com>
2022-04-12 03:47:28 +0530

    finalized signup screen

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -6,40 +6,151 @@ import {
   TouchableHighlight,
   TouchableWithoutFeedback,
 } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GlobalStyles from "../utils/GlobalStyles";
+import ValidateSignUp from "./ValidateSignUp";
+import ErrorModel from "../utils/ErrorModel";
 
 const SignUpForm = ({ navigation }) => {
+  const [loginForm, setLoginForm] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [isModel, setIsModel] = useState(false);
+  const [checkError, setCheckError] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  useEffect(() => {
+    //check if there is an any errors and show the model
+    if (Object.keys(errors).length > 0) {
+      if (errors[Object.keys(errors)[0]] !== "") {
+        setIsModel(true);
+      }
+    }
+  }, [checkError]);
+
+  useEffect(() => {
+    //check if there is an any errors and show the model
+    if (Object.keys(errors).length === 0 && isSubmit) {
+      handelSubmit();
+    }
+  }, [errors]);
+
+  const setValues = (text, name) => {
+    setLoginForm({ ...loginForm, [name]: text });
+  };
+
+  const onSubmit = () => {
+    setErrors(ValidateSignUp(loginForm));
+    setCheckError(!checkError);
+    setIsSubmit(true);
+  };
+
+  const onFocus = (type) => {
+    if (errors[type] && errors[type] !== "") {
+      setErrors({ ...errors, [type]: "" });
+    }
+  };
+
+  const handelSubmit = () => {
+    console.log(errors);
+    console.log("handel submit");
+  };
+
   return (
     <View style={styles.form}>
+      {isModel && (
+        <ErrorModel
+          title="Login Failed"
+          msg={errors}
+          type="Error"
+          setIsModel={setIsModel}
+        />
+      )}
       <View style={styles.inputField}>
-        <TextInput style={styles.inputStyle} placeholder="First Name" />
-      </View>
-      <View style={styles.inputField}>
-        <TextInput style={styles.inputStyle} placeholder="Last Name" />
-      </View>
-      <View style={styles.inputField}>
-        <TextInput style={styles.inputStyle} placeholder="Username" />
-      </View>
-      <View style={styles.inputField}>
-        <TextInput style={styles.inputStyle} placeholder="Phone Number" />
+        <TextInput
+          style={[
+            styles.inputStyle,
+            errors.firstName && styles.inputStyleError,
+          ]}
+          placeholder="First Name"
+          onChangeText={(text) => setValues(text, "firstName")}
+          onFocus={() => onFocus("firstName")}
+        />
       </View>
       <View style={styles.inputField}>
         <TextInput
-          secureTextEntry={true}
-          style={styles.inputStyle}
-          placeholder="Password"
+          style={[styles.inputStyle, errors.lastName && styles.inputStyleError]}
+          placeholder="Last Name"
+          onChangeText={(text) => setValues(text, "lastName")}
+          onFocus={() => onFocus("lastName")}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <TextInput
+          style={[styles.inputStyle, errors.userName && styles.inputStyleError]}
+          placeholder="Username"
+          onChangeText={(text) => setValues(text, "userName")}
+          onFocus={() => onFocus("userName")}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <TextInput
+          style={[styles.inputStyle, errors.email && styles.inputStyleError]}
+          placeholder="Email"
+          onChangeText={(text) => setValues(text, "email")}
+          onFocus={() => onFocus("email")}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <TextInput
+          style={[
+            styles.inputStyle,
+            errors.phoneNumber && styles.inputStyleError,
+          ]}
+          placeholder="Phone Number"
+          onChangeText={(text) => setValues(text, "phoneNumber")}
+          onFocus={() => onFocus("phoneNumber")}
+          keyboardType={"phone-pad"}
         />
       </View>
       <View style={styles.inputField}>
         <TextInput
           secureTextEntry={true}
-          style={styles.inputStyle}
+          style={[styles.inputStyle, errors.password && styles.inputStyleError]}
+          placeholder="Password"
+          onChangeText={(text) => setValues(text, "password")}
+          onFocus={() => onFocus("password")}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <TextInput
+          secureTextEntry={true}
+          style={[
+            styles.inputStyle,
+            errors.confirmPassword && styles.inputStyleError,
+          ]}
           placeholder="Confirm Password"
+          onChangeText={(text) => setValues(text, "confirmPassword")}
+          onFocus={() => onFocus("confirmPassword")}
         />
       </View>
       <View style={styles.formButton}>
-        <TouchableHighlight style={styles.button}>
+        <TouchableHighlight style={styles.button} onPress={onSubmit}>
           <Text style={styles.buttonText}>Sign Up</Text>
         </TouchableHighlight>
         <View style={styles.bottomTextSec}>
@@ -79,6 +190,9 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     width: "100%",
     borderRadius: 8,
+  },
+  inputStyleError: {
+    backgroundColor: "#fff5f5",
   },
   formButton: {
     width: "100%",
---
+++ components/ValidateSignUp.js
@@ -0,0 +1,53 @@
+export default function ValidateSignUp(values) {
+  let formErrors = {};
+  let pattern = new RegExp(
+    /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+  );
+
+  if (values.firstName.trim().length === 0) {
+    formErrors.firstName = "Please Enter First Name";
+  }
+  if (values.firstName.length > 6) {
+    formErrors.firstName = "Fist Name Should be Less Than 6 Characters";
+  }
+  if (values.lastName.trim().length === 0) {
+    formErrors.lastName = "Please Enter Last Name";
+  }
+  if (values.lastName.trim().length > 15) {
+    formErrors.lastName = "Last Name Should be Less Than 15 Characters";
+  }
+  if (values.userName.trim().length === 0) {
+    formErrors.userName = "Please Enter Username";
+  }
+  if (values.userName.trim().length > 8) {
+    formErrors.userName = "Username Should be Less Than 8 Characters";
+  }
+  if (values.email.trim().length === 0) {
+    formErrors.email = "Please Enter Email";
+  } else if (values.email.trim().length > 25) {
+    formErrors.email = "Email Should be Less Than 25 Characters";
+  } else if (!pattern.test(values.email)) {
+    formErrors.email = "Please Enter Valid Email";
+  }
+  if (values.phoneNumber.trim().length === 0) {
+    formErrors.phoneNumber = "Please Enter Phone Number";
+  }
+  if (values.phoneNumber.trim().length > 10) {
+    formErrors.phoneNumber = "Phone Number Should be Less Than 10 Numbers";
+  }
+  if (values.password.trim().length === 0) {
+    formErrors.password = "Please Enter Password";
+  }
+  if (values.password.trim().length > 8) {
+    formErrors.password = "Password should be Less Than 8 Characters";
+  }
+  if (values.confirmPassword.trim().length === 0) {
+    formErrors.confirmPassword = "Please Enter Confirm Password";
+  } else {
+    if (values.password !== values.confirmPassword) {
+      formErrors.confirmPassword = "Confirm Password Invalid";
+    }
+  }
+
+  return formErrors;
+}
