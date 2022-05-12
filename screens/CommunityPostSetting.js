2388cf1588ae88164493d7196ee403f39997bd18
Osada97 <osadamanohara55@gmail.com>
2022-05-12 11:38:37 +0530

    added edit community post

--- screens/CommunityPostSetting.js
+++ screens/CommunityPostSetting.js
@@ -17,9 +17,123 @@ import PostEditHook from "../utils/hook/PostEditHook";
 import CustomHeaderInPostSetting from "../components/CustomHeaderInPostSetting";
 import AddEditImageCoursoul from "../components/AddEditImageCoursoul";
 
-const CommunityPostSetting = () => {
+const CommunityPostSetting = ({ route, navigation }) => {
+  const { id } = route.params;
+  const [details, setDetails] = useState({});
+  const [image, setImage] = useState([]);
+  const [isRefresh, setIsRefresh] = useState(false);
+
+  const { token } = useSelector((state) => state.user);
+
   const { editComment, setPostValue, values, isModel, setIsModel, errors } =
     PostEditHook(details, setImage, handelSubmit);
+
+  useEffect(() => {
+    Axios.get(`${API_KEY}/community/getonepost/${id}`, {
+      headers: { Authorization: "Bearer " + token },
+    })
+      .then((res) => {
+        setDetails({ ...res.data });
+      })
+      .catch((err) => console.log(err));
+  }, [isRefresh]);
+
+  const removeImage = (index) => {
+    //remove images in carousel
+    const imageDetails = image[index];
+
+    //remove images in server
+    if (imageDetails.edited !== undefined) {
+      Alert.alert("Delete Image", "Do you want to delete image on comment", [
+        { text: "Cancel" },
+        {
+          text: "Delete",
+          onPress: () => {
+            Axios.delete(
+              `${API_KEY}/community/removeimageinpost/${imageDetails.id}`,
+              {
+                headers: { Authorization: "Bearer " + token },
+              }
+            )
+              .then(() => {
+                const im = image.filter(
+                  (data) => image.indexOf(data) !== index
+                );
+                setImage([...im]);
+              })
+              .catch((err) => console.log(err));
+          },
+        },
+      ]);
+    } else {
+      //removes newly added images
+      const im = image.filter((data) => image.indexOf(data) !== index);
+      setImage([...im]);
+    }
+  };
+
+  function handelSubmit() {
+    //submit after validating
+    Axios.put(
+      `${API_KEY}/community/updatepost/${details.id}`,
+      {
+        post_title: values.title,
+        description: values.description,
+      },
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }
+    )
+      .then((res) => {
+        Keyboard.dismiss();
+        if (image.length > 0) {
+          imageUpload(res.data);
+        } else {
+          setIsRefresh(!isRefresh);
+          navigation.goBack();
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  const imageUpload = async (res) => {
+    let formData = new FormData();
+
+    for (const img of image) {
+      const split = img.uri.split("/");
+      const filenameWithExt = split[split.length - 1];
+      const mimeType = mime.lookup(filenameWithExt);
+
+      if (img.edited === undefined) {
+        formData.append("file", {
+          uri: img.uri,
+          name: filenameWithExt,
+          type: mimeType,
+        });
+
+        let result = await fetch(
+          `${API_KEY}/community/addimagetopost/${res.id}`,
+          {
+            method: "post",
+            body: formData,
+            headers: {
+              "Content-Type": "multipart/form-data",
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+
+        let responseJson = await result.json();
+
+        if (responseJson) {
+          setIsRefresh(!isRefresh);
+          navigation.goBack();
+        }
+      }
+    }
+  };
 
   return (
     <View style={styles.screen}>
